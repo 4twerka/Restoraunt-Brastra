96eed4ecbfb997ef7ff713dc8592fedcfca7854b
Artem Posudevskyi <penquin4m@gmail.com>
2025-02-19 15:36:12 +0200

    added burger menu and mobile version header

---
+++ src/components/burgerMenu/BurgerMenu.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import searchButton from "../../assets/icons/search.png";
+
+function BurgerMenu({ setIsOpen }) {
+
+    return (
+        <div className="fixed inset-0 bg-black text-white flex items-center justify-center p-4">
+            <button onClick={() => setIsOpen(false)} className="absolute top-4 right-4 text-5xl">&times;</button>
+            <ul className="space-y-6 text-2xl text-center">
+                <li className="hover:text-gray-400 cursor-pointer activeHeader">Home</li>
+                <li className="hover:text-gray-400 cursor-pointer">Restaurant Menu</li>
+                <li className="hover:text-gray-400 cursor-pointer">Wines</li>
+            </ul>
+            <div className="absolute bottom-10 flex items-center border border-gray-600 rounded-lg p-2 w-3/4 max-w-xs mx-auto">
+                <input type="text" className="bg-transparent outline-none text-white flex-1 px-2" />
+                <button className="p-1">
+                    <img src={searchButton} alt="Search" className="w-5 h-5" />
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export { BurgerMenu };
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,28 +1,41 @@
-import React from "react";
-import searchIcon from "../../assets/icons/search.png"
-import logo from "../../assets/icons/logo.png"
+import { useState } from "react";
+import logo from "../../assets/icons/logo.png";
+import searchButton from "../../assets/icons/search.png";
+import { BurgerMenu } from "../burgerMenu/BurgerMenu";
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <header className="bg-black flex items-center justify-between p-3">
-            <div>
-                <img src={logo} alt="logo"/>
-            </div>
+        <header className="bg-black flex items-center justify-between p-4">
+            <img src={logo} alt="logo" className="max-w-[150px]" />
 
-            <ul className="flex gap-5 items-center text-white">
-                <li className="activeHeader cursor-pointer">Home</li>
-                <li className="cursor-pointer">Restoraunt Menu</li>
-                <li className="cursor-pointer">Wines</li>
-            </ul>
+            <nav className="hidden md:flex gap-6 text-gray-300">
+                <a className="text-red-500 underline cursor-pointer">Home</a>
+                <a className="cursor-pointer">Restaurant Menu</a>
+                <a className="cursor-pointer">Wines</a>
+            </nav>
 
-            <div className="flex items-center">
-                <input className="text-white bg-black border-2 p-3 rounded-3xl outline-none" type="text" placeholder="Find Great Food"/>
-                <button className="text-white">
-                    <img src={searchIcon} alt="search" />
+            <div className="hidden md:flex items-center border border-white rounded-full px-5 h-10 max-w-sm">
+                <input 
+                    className="bg-black text-white outline-none placeholder-gray-400 flex-1"
+                    type="text" 
+                    placeholder="Find Great Food"
+                />
+                <button className="flex items-center justify-center w-10 h-10">
+                    <img src={searchButton} alt="search" />
                 </button>
             </div>
+
+            <div onClick={() => setIsOpen((prev) => !prev)} className="md:hidden flex flex-col gap-1 cursor-pointer">
+                <span className="w-6 h-0.5 bg-white block"></span>
+                <span className="w-6 h-0.5 bg-white block"></span>
+                <span className="w-6 h-0.5 bg-white block"></span>
+            </div>
+
+            {isOpen && <BurgerMenu setIsOpen={setIsOpen} />}
         </header>
     );
 }
 
-export { Header }
+export { Header };
